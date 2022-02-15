bef49e8cbbf0e72b9b91bec970a6452547f163dd
Josh <c00224660@itcarlow.ie>
2022-02-15 17:36:40 +0000

    Products are now read in from Commerce.js website account products

--- Project_Files/my-app/src/App.js
+++ Project_Files/my-app/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Products from './components/products/Products';
 import Navbar from './components/navbar/Navbar'
 
+import { commerce } from './lib/Commerce';
+
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
         <Navbar />
-        <Products />
+        <Products products={products}/>
     </div>
   );
 };
